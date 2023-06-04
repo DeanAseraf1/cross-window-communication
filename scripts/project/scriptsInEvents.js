cc379db001eabbbbb61da2d331bf1beae84eb9e6
Dean Yair Aseraf <90892852+DeanAseraf1@users.noreply.github.com>
2023-06-04 13:45:44 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+var postMessage = '';
+function postText(value){
+console.log("IFrame Sender: " + value);
+	parent.postMessage(value, "*");
+
+}
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event1_Act2(runtime, localVars)
+	{
+		postText(runtime.globalVars.postMessage);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
